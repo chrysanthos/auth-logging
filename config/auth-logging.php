d045eba51bc737dd820221da19e5835a179fc55c
Chrysanthos Prodromou <chrysanthos.prodromou@gmail.com>
2020-01-11 01:39:52 +0200

    Add value masking

--- config/auth-logging.php
+++ config/auth-logging.php
@@ -5,4 +5,16 @@ return [
 
     'sync' => true,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Masked fields
+    |--------------------------------------------------------------------------
+    |
+    | The values to be masked before inserted into the database.
+    | Available columns to be masked: email, password
+    |
+    */
+
+    'mask' => ['password'],
+
 ];
--- src/Jobs/LogAuthAction.php
+++ src/Jobs/LogAuthAction.php
@@ -33,16 +33,58 @@ class LogAuthAction implements ShouldQueue
      */
     public function handle()
     {
-        $email = $this->credentials['email'] ?? $this->credentials['username'] ?? null;
+        $email = $this->getEmail();
 
         $existing_user = DB::table('users')->where('email', $email)->exists();
 
         AuthLogEntry::create([
             'email'         => $email,
-            'password'      => $this->credentials['password'],
+            'password'      => $this->getPassword(),
             'ip'            => $this->credentials['ip'],
             'user_agent'    => $this->credentials['user_agent'],
             'existing_user' => $existing_user,
         ]);
+    }
+
+    /**
+     * @return string|null
+     */
+    protected function getEmail()
+    {
+        $value = $this->credentials['email'] ?? $this->credentials['username'] ?? null;
+
+        if ($value === null) {
+            return null;
+        }
+
+        $maskedFields = config('auth-logging.mask', []);
+
+        return in_array('email', $maskedFields, true)
+            ? $this->mask($value, (int) (strlen($value) / 2))
+            : $this->credentials['email'];
+    }
+
+    /**
+     * @return string
+     */
+    protected function getPassword()
+    {
+        $value        = $this->credentials['password'];
+        $maskedFields = config('auth-logging.mask', []);
+
+        return in_array('password', $maskedFields, true)
+            ? $this->mask($value, (int) (strlen($value) / 2))
+            : $this->credentials['password'];
+    }
+
+    /**
+     * @param  string  $value  The value to be masked
+     * @param  int  $number  The number of characters to be plain text
+     *
+     * @return string
+     */
+    protected function mask(string $value, $number)
+    {
+        return str_repeat('*', strlen($value) - $number).substr($value, -$number);
     }
 }
